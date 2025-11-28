912880dbf0b712db4e961fc2020535ccc7df6d92
MadanDhungel <68050529+MadanDhungel@users.noreply.github.com>
2025-11-28 15:38:09 +0545

    Remove HTTP server implementation from index1.js

--- index1.js
+++ index1.js
@@ -1,13 +1,1 @@
-const http = require('http');
 
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('\n');
-});
-
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
